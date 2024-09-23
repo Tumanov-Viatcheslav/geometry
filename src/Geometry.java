496d38bd24aff30182d756e049e6a8d026033a49
Tumanov <tslavich@bk.ru>
2024-09-23 15:51:28 +0300

    Cosmetic

--- src/Geometry.java
+++ src/Geometry.java
@@ -5,11 +5,11 @@ import figures.Triangle;
 public class Geometry {
     public static void main(String[] args) {
         Circle c = new Circle(2, 0, 1);
-        System.out.println(c.toString() + "\nperimeter=" + c.perimeter() + " area=" + c.area());
+        System.out.println(c + "\nperimeter=" + c.perimeter() + " area=" + c.area());
         Triangle tr1 = new Triangle(0, 0, 3, 0, 0, 4);
-        System.out.println(tr1.toString() + "\nisRight=" + tr1.isRight() + " isIsosceles=" + tr1.isIsosceles() + " isEquilateral=" + tr1.isEquilateral() +
+        System.out.println(tr1 + "\nisRight=" + tr1.isRight() + " isIsosceles=" + tr1.isIsosceles() + " isEquilateral=" + tr1.isEquilateral() +
                             "\nperimeter=" + tr1.perimeter() + " area=" + tr1.area());
         Quadrangle quad = new Quadrangle(0,0,1,0,1,1,0,1);
-        System.out.println(quad.toString() + "\nisRectangle=" + quad.isRectangle());
+        System.out.println(quad + "\nisRectangle=" + quad.isRectangle());
     }
 }
